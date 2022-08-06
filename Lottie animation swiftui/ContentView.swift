c21f76c61eaeb252983dc7ea71a433c09b4e47b6
Kevin Ahmad <kevinahmad.f@gmail.com>
2022-08-06 23:23:18 +0700

    Initial Commit

---
+++ Lottie animation swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lottie animation swiftui
+//
+//  Created by Kevin ahmad on 06/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lottie animation swiftui/Lottie_animation_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lottie_animation_swiftuiApp.swift
+//  Lottie animation swiftui
+//
+//  Created by Kevin ahmad on 06/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Lottie_animation_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
